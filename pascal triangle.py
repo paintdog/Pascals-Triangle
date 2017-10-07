47fbc729b536a7ef0e33c9722e1445d0a9534f5c
paintdog <paintdog@users.noreply.github.com>
2017-10-07 23:44:47 +0200

    Add files via upload

---
+++ pascal triangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def get_pascal_triangle(n):    
+    table = []
+    for i in range(n):
+        if i == 0:
+            table.append([1])
+        elif i == 1:
+            table.append([1, 1])
+        else:
+            line = []
+            for i, item in enumerate(table[-1][:-1]):
+                line.append(table[-1][i] + table[-1][i + 1])
+            table.append([1] + line + [1])
+    return table
+
+
+def output_triangle(triangle):
+    for line in triangle:
+        width = len(" ".join(map(str, triangle[-1])))
+        print(" ".join(map(str, line)).center(width, " "))
+    
+
+if __name__ == "__main__":
+    n = 5
+    triangle = get_pascal_triangle(n)
+    output_triangle(triangle)
